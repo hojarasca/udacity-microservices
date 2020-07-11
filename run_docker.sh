c816d2db9c8d510a2f76ed1a649a7378fabadc16
hojarasca <miguelfeliped@gmail.com>
2020-07-10 23:51:04 -0300

    run docker working

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t udacity-predictions .
 
 # Step 2: 
 # List docker images
+docker image ls -f "reference=udacity-predictions:*"
 
 # Step 3: 
 # Run flask app
+docker run --rm --name udacity-predictions -p 8090:80 udacity-predictions
