4389f45e3dc9668e48053afc65203636b78c6caf
hojarasca <miguelfeliped@gmail.com>
2020-07-11 00:52:15 -0300

    kubernetes.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath='hojarasca/udacity-predictions'
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-predictions --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment udacity-predictions --type=NodePort --port=80
+kubectl get services
 
